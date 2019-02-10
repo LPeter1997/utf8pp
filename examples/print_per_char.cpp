8d65e374ca7d3637dc4293ea5f7778583307cac2
LPeter1997 <lenkefi.peti@gmail.com>
2019-02-10 17:04:21 +0100

    Made parsing and reading backwards

--- examples/print_per_char.cpp
+++ examples/print_per_char.cpp
@@ -4,11 +4,26 @@
 int main() {
     utf8pp::set_stdout_utf8();
     char const* str = u8"こんにちは世界";
-    utf8pp::utf8_byte const* ustr = (utf8pp::utf8_byte const*)str;
-    while (auto skp = utf8pp::parse_next(ustr)) {
-        if (skp < 0) return 1;
-        std::printf("%.*s\n", int(skp), (char const*)ustr);
-        ustr += skp;
+    utf8pp::utf8_byte const* ustr_start = (utf8pp::utf8_byte const*)str;
+    utf8pp::utf8_byte const* ustr = ustr_start;
+
+    std::puts("Forward:");
+    {
+        while (auto skp = utf8pp::parse_next(ustr)) {
+            if (skp < 0) return 1;
+            std::printf(" %.*s\n", int(skp), (char const*)ustr);
+            ustr += skp;
+        }
     }
+
+    std::puts("Backward:");
+    {
+        while (auto skp = utf8pp::parse_prev(ustr_start, ustr)) {
+            if (skp < 0) return 1;
+            ustr -= skp;
+            std::printf(" %.*s\n", int(skp), (char const*)ustr);
+        }
+    }
+
     return 0;
 }
--- src/utf8pp.hpp
+++ src/utf8pp.hpp
@@ -87,27 +87,23 @@ calc_codepoint(utf8_byte const* src, utf8_usize len) noexcept {
         "The length of a unicode codepoint ban only be 1 to 4 bytes!");
 
     switch (len) {
-    case 1: {
-        return utf8_cp(src[0]);
-    } break;
+    case 1:
+        return   utf8_cp(src[0]);
 
-    case 2: {
+    case 2:
         return ((utf8_cp(src[0]) & 0b00011111) << 6)
-             | (utf8_cp(src[1]) & 0b00111111);
-    } break;
+             |  (utf8_cp(src[1]) & 0b00111111);
 
-    case 3: {
+    case 3:
         return ((utf8_cp(src[0]) & 0b00001111) << 12)
              | ((utf8_cp(src[1]) & 0b00111111) << 6)
-             | (utf8_cp(src[2]) & 0b00111111);
-    } break;
+             |  (utf8_cp(src[2]) & 0b00111111);
 
-    case 4: {
+    case 4:
         return ((utf8_cp(src[0]) & 0b00000111) << 18)
              | ((utf8_cp(src[1]) & 0b00111111) << 12)
              | ((utf8_cp(src[2]) & 0b00111111) << 6)
-             | (utf8_cp(src[3]) & 0b00111111);
-    } break;
+             |  (utf8_cp(src[3]) & 0b00111111);
     }
 
     utf8pp_panic("Unreachable!");
@@ -142,14 +138,10 @@ inline /* constexpr */ utf8_ssize parse_next(utf8_byte const* src) noexcept {
     if (src[0] < 0b11000000 || src[0] > 0b11110111) return error::invalid_utf8;
     // 2-byte encoding
     if (detail::next_byte_bad(src[1])) return error::invalid_utf8;
-    if (src[0] < 0b11100000) {
-        return 2;
-    }
+    if (src[0] < 0b11100000) return 2;
     // 3-byte encoding
     if (detail::next_byte_bad(src[2])) return error::invalid_utf8;
-    if (src[0] < 0b11110000) {
-        return 3;
-    }
+    if (src[0] < 0b11110000) return 3;
     // 4-byte encoding
     if (detail::next_byte_bad(src[3])) return error::invalid_utf8;
     return 4;
@@ -170,6 +162,58 @@ read_next(utf8_byte const* src, utf8_cp& dest) noexcept {
     if (result <= 0) return result;
 
     dest = detail::calc_codepoint(src, result);
+    return result;
+}
+
+/**
+ * Same as @see parse_next, but parses backwards the first UTF8 character
+ * *before* the parse position.
+ * @param src_start The start of the entire string.
+ * @param src @see parse_next
+ * @return @see parse_next. If src_start == src, then the return value is zero.
+ */
+inline /* constexpr */ utf8_ssize
+parse_prev(utf8_byte const* src_start, utf8_byte const* src) noexcept {
+    if (src_start == src) return 0;
+    if (src[-1] == '\0') return 0;
+    // At least one byte long
+    // Single-byte
+    if (src[-1] < 0b10000000) return 1;
+    // 2-byte encoding
+    if (src_start == src - 1) return error::invalid_utf8;
+    if (detail::next_byte_bad(src[-1])) return error::invalid_utf8;
+    if (detail::next_byte_bad(src[-2])) {
+        if (src[-2] < 0b11000000 || src[-2] > 0b11011111)
+            return error::invalid_utf8;
+        return 2;
+    }
+    // 3-byte encoding
+    if (src_start == src - 2) return error::invalid_utf8;
+    if (detail::next_byte_bad(src[-2])) return error::invalid_utf8;
+    if (detail::next_byte_bad(src[-3])) {
+        if (src[-3] < 0b11100000 || src[-3] > 0b11101111)
+            return error::invalid_utf8;
+        return 3;
+    }
+    // 4-byte encoding
+    if (src_start == src - 3) return error::invalid_utf8;
+    if (src[-4] < 0b11110000 || src[-4] > 0b11110111)
+        return error::invalid_utf8;
+    return 4;
+}
+
+/**
+ * Same as @see read_next but with @see parse_prev.
+ */
+inline /* constexpr */ utf8_ssize
+read_prev(utf8_byte const* src_start, utf8_byte const* src,
+    utf8_cp& dest) noexcept {
+    dest = '\0';
+
+    auto result = parse_prev(src_start, src);
+    if (result <= 0) return result;
+
+    dest = detail::calc_codepoint(src - result, result);
     return result;
 }
 
